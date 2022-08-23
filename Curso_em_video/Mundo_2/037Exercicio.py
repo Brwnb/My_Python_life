7c58068a75522351252a8c89d4700b35a49a255f
Bruno Borges <bruno2.0oliveira@gmail.com>
2022-08-23 19:53:28 +0100

    Exercise 037

--- Curso_em_video/Mundo_2/037Exercicio.py
+++ Curso_em_video/Mundo_2/037Exercicio.py
@@ -4,3 +4,19 @@
 # - 1 para binário
 # - 2 para octal
 # - 3 para hexadecimal
+
+print("Escolha em qual formato você quer converter os valores digitados:")
+print("==hex para Hexadecimal==")
+print("==bin para binario==")
+print("==oc para octal==")
+verifica = input("Entra com uma das oções acima: ")
+value = int(input("Enter With A value: "))
+
+if verifica == "bin":
+	print("The binary Value is: {}".format(format(value,"b")))
+elif verifica == "oc":
+	print("The Octal Value is: {}".format(format(value,"o")))
+elif verifica == "hex":
+	print("The Hex Value is: {}".format(format(value,"x")))
+else:
+	print("Nenhum valor válido")
