3e39997f878a25edc1a82c13911f27eefccac8d1
Bruno Borges <bruno2.0oliveira@gmail.com>
2022-08-23 20:59:57 +0100

    038exercicio it has made

--- Curso_em_video/Mundo_2/038Exercicio.py
+++ Curso_em_video/Mundo_2/038Exercicio.py
@@ -5,3 +5,13 @@
 # - O segundo valor é maior
 # - Não existe valor maior, os dois são iguais.
 #
+
+value1 = int(input("Enter with the first value: "))
+value2 = int(input("Enter with the second value: "))
+
+if value1 < value2:
+	print("The number {} is minor than {}".format(value1, value2))
+elif value1 > value2:
+	print("The number {} is major than {}".format(value1, value2))
+else:
+	print("They are iquals")
