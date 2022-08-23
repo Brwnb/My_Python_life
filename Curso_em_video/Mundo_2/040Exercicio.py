53f747cd9bbbae6215a741e8cfcad6eea31c6d0e
Bruno Borges <bruno2.0oliveira@gmail.com>
2022-08-23 23:28:18 +0100

    040Exercise

--- Curso_em_video/Mundo_2/040Exercicio.py
+++ Curso_em_video/Mundo_2/040Exercicio.py
@@ -6,3 +6,14 @@
 # - média entre 5.0 e 6.9 : Recuperação
 # - média 7.0 ou superior: Aprovado
 
+value1 = float(input("Enter with the first score: "))
+value2 = float(input("Enter with the second score: "))
+
+avg = (value1 + value2)/2
+
+if avg < 5:
+	print("You failed. Your score was {} ".format(avg))
+elif avg >5 and avg < 7:
+	print("You are recovering. Your score was {} ".format(avg))
+elif avg >=7:
+	print("You passed. You score was {}".format(avg))
