4ce4aff8bed89a36234e8400aa2d50401a8f3ba5
Bruno Borges <bruno2.0oliveira@gmail.com>
2022-08-23 23:58:15 +0100

    041 Exercise has done

--- Curso_em_video/Mundo_2/041Exercicio.py
+++ Curso_em_video/Mundo_2/041Exercicio.py
@@ -6,3 +6,18 @@
 # Até 19 anos: junior
 # Até 20 anos: Sênior
 # Acima: Master
+
+age = int(input("Enter with the birth year: "))
+
+if age < 10 and age >0:
+	print("The kid will be classifield as mirim")
+elif age >=10 and age <15:
+	print("The kid will be classifield as infatil")
+elif age >= 15 and age < 20:
+	print("The youth will be classifield as Junior")
+elif age == 20:
+	print("The youth will be classfield as Sênior")
+elif age > 20:
+	print("The youth will be classifield as Master")
+else:
+	print("Enter with a valid value")
