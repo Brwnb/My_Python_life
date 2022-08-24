7a7afd69e9470ce021cf3bd267961a2aeb425a06
Bruno Borges <bruno2.0oliveira@gmail.com>
2022-08-24 18:17:23 +0100

    Exercises I've done

--- Curso_em_video/Mundo_2/050Exercicio.py
+++ Curso_em_video/Mundo_2/050Exercicio.py
@@ -1,3 +1,10 @@
 #050Exercicio.py
 #Deselvolva um programa que leia 6 numeros inteiros e mostre a soma apenas daqueles que 
 #forem pares. Se o valor digitado for impar desconsidere-o
+
+plus = 0
+for i in range (6):
+	value = int(input("Enter with a value: "))
+	if value % 2 == 0:
+			plus = plus + value
+print("The sum of pair numbers are {}".format(plus))
