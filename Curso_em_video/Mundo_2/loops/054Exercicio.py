58b88ff4a6cbfe1a1e4a226666472eba179a95d6
Bruno Borges <bruno2.0oliveira@gmail.com>
2022-08-26 09:27:14 +0100

    Doing 054 Exercise

--- Curso_em_video/Mundo_2/loops/054Exercicio.py
+++ Curso_em_video/Mundo_2/loops/054Exercicio.py
@@ -1,4 +1,16 @@
 #054Exercicio.py
 #Crie um programa que leia o ano de nascimento de 7 pessoas.
 # No final, mostre quantas pessoas ainda não atingiram a maioridade e quantas 
-#já são maiores
+#já são maiores. A maior idade será com 21 anos.
+
+count_majority
+count_minority
+for i in range(7):
+	value = int(input("Enter with a value: "))
+	if value < 21:
+		count_minority += 1
+	else:
+		count_majority
+
+print("The quantity of Majority is {}. ".format(count_majority))
+print("The quantity fo Minority is {}".format(count_minority))
