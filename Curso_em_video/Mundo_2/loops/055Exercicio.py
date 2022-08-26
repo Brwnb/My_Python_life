f073ad00c9e40eab38d3fef421cfd26893e0d579
Bruno Borges <bruno2.0oliveira@gmail.com>
2022-08-27 00:42:11 +0100

    Exercicio 55

--- Curso_em_video/Mundo_2/loops/055Exercicio.py
+++ Curso_em_video/Mundo_2/loops/055Exercicio.py
@@ -1,3 +1,19 @@
 #055Exercicio.py
 #Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o 
 #maior e o menor peso lidos.
+maior = 0
+menor = 0
+for i in range(1, 6):
+	weigth = float(input("Enter with a weigth: "))
+	if i == 1:
+		maior = weigth
+		menor = weigth
+	else:
+		if weigth > maior:
+			maior = weigth
+		if weigth < menor:
+			menor = weigth
+
+
+print("O maior peso lido foi de {} kg" .format(maior))
+print("O menor peso lido foi de {} kg" .format(menor))
