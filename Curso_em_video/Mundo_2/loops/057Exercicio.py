5937f9d2cf8f1b90013da1864f97b00fdb27f7f2
Bruno Borges <bruno2.0oliveira@gmail.com>
2022-08-27 21:12:10 +0100

    Exercise 057

--- Curso_em_video/Mundo_2/loops/057Exercicio.py
+++ Curso_em_video/Mundo_2/loops/057Exercicio.py
@@ -1,3 +1,14 @@
 #057Exercicio.py
 # Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores "M" ou "F"
 #Caso esteja errado, peça a digitação novamente do valor correto.
+
+run = True
+while run:
+	sex = input("Enter with the sex (use F or M): ").upper()
+	if sex == 'M' or sex == 'F':
+		run = False
+	else :
+		print("Enter with the corrected word!!")
+		print("Use F or M")
+		run = True
+
