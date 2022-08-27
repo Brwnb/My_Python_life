b4d6b514526e81bdf00c70d1370b8eaa7b2565b6
Bruno Borges <bruno2.0oliveira@gmail.com>
2022-08-27 23:58:00 +0100

    058 Exercise

--- Curso_em_video/Mundo_2/loops/058Exercicio.py
+++ Curso_em_video/Mundo_2/loops/058Exercicio.py
@@ -1,3 +1,17 @@
 #058Exercicio.py
 #Adivinhe o número que o computador esta pensando.
 # Os números vão de 0 a 10. Tente até conseguir acertar.
+
+from random import randint
+
+sort = randint(0, 10)
+run = True
+while run:
+	value = int(input("Enter with a value between 0 and 10: "))
+	if value == sort:
+		print("CONGRATS. YOU WON!!!")
+		run = False
+	else:
+		print("Try Again")
+		run = True
+
