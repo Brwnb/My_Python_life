4f5b6a58fd1da2b630bc05d59d953c90951159c2
Bruno Borges <bruno2.0oliveira@gmail.com>
2022-08-28 13:50:42 +0100

    Exercise 061 and 062

--- Curso_em_video/Mundo_2/loops/061Exercicio.py
+++ Curso_em_video/Mundo_2/loops/061Exercicio.py
@@ -1,2 +1,12 @@
 #Faça uma progreção aritimetica lendo o primeiro termo e a sua razão. 
 #mostrando os 10 primeiros termos da progressão usando a estrutura while.
+
+term = int(input("Enter with the first term: "))
+ratio = int(input("Enter with the ratio: "))
+
+n = 0
+while n < 10:
+	print(term)
+	term = term + ratio
+	n += 1
+
--- Curso_em_video/Mundo_2/loops/062Exercicio.py
+++ Curso_em_video/Mundo_2/loops/062Exercicio.py
@@ -1,3 +1,23 @@
 #062Exercicio.py
 # Refaça o exercicio 061 e pergunte ao usuário se ele quer mais alguns termos.
 # Para sair o usuário deve digitar 0 em termos.
+
+
+n = 0
+count = 0
+while True:
+	if count == 0:
+		value = int(input("If you want to exit enter with 0 :"))
+		if value == 0:
+			break
+		else:
+			count = 1
+	elif count != 0:
+		term = int(input("Enter with the first term: "))
+		ratio = int(input("Enter with the ratio: "))
+		count = 0
+		n = 0
+		while n < 10:
+			print(term)
+			term = term + ratio
+			n += 1
