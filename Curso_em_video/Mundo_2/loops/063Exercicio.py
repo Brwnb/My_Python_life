8d9fece8c25c899df1ef486f47a35b6620c324db
Bruno Borges <bruno2.0oliveira@gmail.com>
2022-08-28 14:39:26 +0100

    Change 063 exercise

--- Curso_em_video/Mundo_2/loops/063Exercicio.py
+++ Curso_em_video/Mundo_2/loops/063Exercicio.py
@@ -1,3 +1,14 @@
 #063Exercicio.py
 # Escreva um programa que leia um número n inteiro qualquer e mostre na tela os
 # n primeiros elementos de uma Sequência de Fibonacci.
+
+terms = int(input("Enter with a value: "))
+n1, n2 =0, 1
+count = 0
+while count < terms:
+	value = n1 + n2
+	n1 = n2
+	n2 = value
+	count += 1
+
+print(value)
