d5ba2896a44ad9cc79cd9e0a0b3c3913aae0da14
Bruno Borges <bruno2.0oliveira@gmail.com>
2022-08-28 01:53:42 +0100

    Exercicio 060

--- Curso_em_video/Mundo_2/loops/060Exercicio.py
+++ Curso_em_video/Mundo_2/loops/060Exercicio.py
@@ -1,2 +1,11 @@
 #060Exercicio.py
 # Faça um programa que leia um número qualquer e mostre o seu fatorial.
+
+value = int(input("Enter with a number: "))
+
+fatorial = 1
+while value > 1:
+	fatorial = fatorial * value
+	value -= 1
+
+print(fatorial)
