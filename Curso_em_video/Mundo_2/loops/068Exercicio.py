009d5e18493bd40f335087dcf789ad01c4343138
Bruno Borges <bruno2.0oliveira@gmail.com>
2022-08-29 12:06:07 +0100

    068 corrected

--- Curso_em_video/Mundo_2/loops/068Exercicio.py
+++ Curso_em_video/Mundo_2/loops/068Exercicio.py
@@ -1,7 +1,7 @@
 #Faça um programa que jogue par ou ímpar com o computador.
 # O jogo só será interrompido quando o jogador perder, mostrando o total de 
 #vitórias consecutivas que ele conquistou no final do jogo. 
-
+"""
 from random import choice
 choices = ''
 temp = ['PAR','IMPAR']
@@ -15,3 +15,32 @@ while True:
 		print("Sorry!! You lose!!")
 		break
 print(f"The loop worked {count} times")
+"""
+
+from random import randint
+v = 0
+while True:
+	player = int(input("Enter with  a value between 0 and 5 (one hand okay!!!): "))
+	cpu = randint(0 ,5)
+	total = cpu + player
+	choises = ' '
+	while choises not in 'EeOo':
+		choises = input("Even or Odd? ").upper().strip()[0]
+	print(f'You played {player} and the computer played {cpu}. The Total is {total}')
+	print('Came out even' if total % 2 == 0 else 'came out Odd')
+	if choises == 'E':
+		if total % 2 == 0:
+			print("You Won!!!")
+			v += 1
+		else:
+			print("You lose")
+			break
+	elif choises == 'O':
+		if total % 2 == 1:
+			print("You Won!!!")
+			v += 1
+		else:
+			print("You lose")
+			break
+	print("Let's play again")
+print(f"Game Over. You won {v} time(s)")
