356a418f34e6d99f391cc6942531c0b339b4aa31
Bruno Borges <bruno2.0oliveira@gmail.com>
2022-08-31 12:07:13 +0100

    Finalizado o exercicio 78

--- Curso_em_video/Mundo_3/listas/078Exercicio.py
+++ Curso_em_video/Mundo_3/listas/078Exercicio.py
@@ -2,9 +2,33 @@
 #Exercício Python 078: Faça um programa que leia 5 valores numéricos e guarde-os em uma lista. 
 #No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na 
 #lista.
+
+maior = menor = 0
 value = list()
 for i in range(5):
 	value.append(int(input("Digite alguns valores: ")))
+	if i == 0:
+		menor = maior = value[i]
+	else:
+		if value[i] > maior:
+			maior = value[i]
+		if value[i] < menor:
+			menor = value[i]
 
-print(f"O maior valor é {max(value)}")
-print(f"O menor valor é {min(value)}")
+print(f"Valores digitados{value}")
+print(f"O maior valor é {maior} nas posições ", end ="")
+for a,b in enumerate(value):
+	if b == maior:
+		print(f"{a}, ", end="")
+print()
+
+print(f"O menor valor é {menor} nas posições ", end="")
+for a,b in enumerate(value):
+	if b == menor:
+		print(f"{a}, ", end="")
+
+print()
+
+# Usando funções do python
+#print(f"O maior valor é {max(value)}")
+#print(f"O menor valor é {min(value)}")
