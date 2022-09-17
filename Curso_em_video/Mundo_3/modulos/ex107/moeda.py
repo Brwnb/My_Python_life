dcc7aac2f7dfc93ca7fc42f0d3bf23152f7b2862
Bruno Borges <bruno2.0oliveira@gmail.com>
2022-09-17 15:48:26 +0100

    Deleting files

---
+++ Curso_em_video/Mundo_3/modulos/ex107/moeda.py
@@ -0,0 +1,18 @@
+def aumentar(preco, taxa):
+	result = preco + (preco * taxa/100)
+	return result
+
+
+def diminuir(preco, taxa):
+	result = preco  - (preco * taxa/100)
+	return result
+
+
+def dobro(preco):
+	result = preco + preco
+	return result
+
+
+def metade(preco):
+	result = preco / 2
+	return result
